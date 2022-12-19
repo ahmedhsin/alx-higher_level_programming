5edf7fd6dbbe5ebdb44c973f2e37717dcd29514f
Yuu <ahmed.elia2003@gmail.com>
2022-12-19 18:11:03 +0200

    final mand tasks

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -9,11 +9,13 @@ def list_division(my_list_1, my_list_2, list_length):
                 new_list.append(0)
         except ZeroDivisionError:
             print("division by 0")
+            new_list.append(0)
         except ValueError:
             print("wrong type")
+            new_list.append(0)
         except IndexError:
             print("out of range")
             break
         finally:
-            new_list.append(0)
+            continue
     return new_list
