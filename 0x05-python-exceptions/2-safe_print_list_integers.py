246fc9b20374f5b4c856481359d1ff739c7124e8
Yuu <ahmed.elia2003@gmail.com>
2022-12-19 16:46:21 +0200

    task 2 fix

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     counter = 0
+    fals = 0
     while counter < x:
         try:
             print("{:d}".format(my_list[counter]), end="")
@@ -8,6 +9,8 @@ def safe_print_list_integers(my_list=[], x=0):
         except IndexError:
             break
         except Exception:
-            continue
+            counter += 1
+            fals += 1
+            pass
     print("")
-    return counter
+    return counter - fals
