e8ce79896e91afd65eddbd312718e0e195cdc652
Yuu <ahmed.elia2003@gmail.com>
2022-12-21 16:49:21 +0200

    docs

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Define A calss name square"""
 class Square:
     """Square class"""
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""Define a class called Square"""
 class Square:
-    """Square class"""
+    """Represent of Square class
+        Attributes:
+           __size (int): size of square
+    """
     def __init__(self, size):
+        """initialize instance"""
         self.__size = size
