f24846bd047d5774b2f6168e4ccaea0b04207b1b
Yuu <ahmed.elia2003@gmail.com>
2022-12-22 01:28:33 +0200

    fix l task

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -68,6 +68,8 @@ class Square:
         if self.__size == 0:
             print("")
         for i in range(self.__size):
+            for k in range(self.__position[0]):
+                print(" ", end="")
             for j in range(self.__size):
                 print("#", end="")
             print()
