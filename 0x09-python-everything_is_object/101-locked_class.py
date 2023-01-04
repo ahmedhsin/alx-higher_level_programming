1769d5e66c9ecb72d4e279173387de41d0609d82
Yuu <ahmed.elia2003@gmail.com>
2023-01-04 03:07:02 +0200

    ts

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""
+    locked class
+"""
 class LockedClass:
+    """just class"""
     def __setattr__(self, name, value):
+        """built in method"""
         if name != "first_name":
             raise AttributeError("'LockedClass' object has no attribute '" + name + "'")
