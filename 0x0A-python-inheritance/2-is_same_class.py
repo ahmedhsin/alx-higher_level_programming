a755d842fa4a51c5959dddfe4b807fe99cbf86ed
Yuu <ahmed.elia2003@gmail.com>
2023-01-10 02:15:55 +0200

    fix bug task 2

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -5,17 +5,7 @@
 def is_same_class(obj, a_class):
     """check if obj is instance of specif class"""
 
-    try:
-        state = isinstance(obj, a_class)
-    except TypeError:
-        try:
-            state = isinstance(a_class, obj)
-        except Exception:
-            state = False
-    except Exception:
-        state = False
-
-    if (state):
+    if (type(obj) == a_class):
         return True
     else:
         return False
