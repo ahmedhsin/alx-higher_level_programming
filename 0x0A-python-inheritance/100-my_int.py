4fe71192c35abaf6e0f4b79bb5fd14d95dc62db2
Yuu <ahmed.elia2003@gmail.com>
2023-01-10 03:53:57 +0200

    adv task 1

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -3,19 +3,11 @@
 
 
 class MyInt(int):
-    def __init__(self, x):
-        if (type(x) != int):
-            raise TypeError
-        self.__ans = x
-
-    def __str__(self):
-        """overload str"""
-        return "{}".format(self.__ans)
-
+    """class"""
     def __eq__(self, other):
         """overload equ"""
-        return not (self.__ans == other)
+        return not (super() == other)
 
     def __ne__(self, other):
         """overlaod nequ"""
-        return (self.__ans == other)
+        return (super() == other)
