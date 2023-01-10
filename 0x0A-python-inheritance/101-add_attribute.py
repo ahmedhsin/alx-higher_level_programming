97bbfb43ed0bb046dd6fa57d1cba0d0578b7d2e4
Yuu <ahmed.elia2003@gmail.com>
2023-01-10 04:10:46 +0200

    last adv

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -3,6 +3,7 @@
 
 
 def add_attribute(obj, name, value):
+    """func to ADD atrr"""
     if hasattr(obj, '__dict__') not True:
         raise TypeError("can't add new attribute")
     obj.__dict__[name] = value
