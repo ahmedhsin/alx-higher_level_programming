55a9626a9f16fd18ccd999aaae271eed37240a94
Yuu <ahmed.elia2003@gmail.com>
2023-01-10 03:18:18 +0200

    11 task

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""classgeo file"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square Class"""
+
+    def __init__(self, size):
+        super().integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """ovrride str fun"""
+        return "[Square] {}/{}".format(self.__width, self.__height)
