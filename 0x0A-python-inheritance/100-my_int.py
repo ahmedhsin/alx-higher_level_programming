b8892feebd63edb07f6657bce1bf056559a572a0
Yuu <ahmed.elia2003@gmail.com>
2023-01-10 03:56:27 +0200

    f

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -6,8 +6,8 @@ class MyInt(int):
     """class"""
     def __eq__(self, other):
         """overload equ"""
-        return not (super() == other)
+        return super().__ne__(other)
 
     def __ne__(self, other):
         """overlaod nequ"""
-        return (super() == other)
+        return super().__eq__(other)
