1cb3476c336ec5b4081c95b5f050389cba587907
Yuu <ahmed.elia2003@gmail.com>
2023-01-10 02:03:56 +0200

    2-task

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""func file"""
+
+
+def is_same_class(obj, a_class):
+    """check if obj is instance of specif class"""
+
+    if (isinstance(obj,a_class)):
+        return True
+    else:
+        return False
