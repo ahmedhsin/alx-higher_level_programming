6a3cc05f23446536480c554cbd201202fcdd921c
Yuu <ahmed.elia2003@gmail.com>
2023-01-11 03:03:43 +0200

    test 4, 5

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import json
+"""Task Answer"""
+
+
+def to_json_string(my_obj):
+    """json to object"""
+    data = json.dumps(my_obj)
+    return data
---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import json
+"""Task Answer"""
+
+
+def from_json_string(my_str):
+    """json to object"""
+    data = json.loads(my_str)
+    return data
