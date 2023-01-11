8a3900825565ccb345bbeb459ece41c1e73ec574
Yuu <ahmed.elia2003@gmail.com>
2023-01-11 04:21:59 +0200

    task -f 8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """Task Answer"""
-import json
 
 
 def class_to_json(obj):
     """class to json"""
 
 
-    return json.loads(json.dumps(obj.__dict__))
+    return obj.__dict__
