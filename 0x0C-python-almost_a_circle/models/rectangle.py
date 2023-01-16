9a977c3b6e8b64c334d703661c86e6b5c9a2fcc0
Yuu <ahmed.elia2003@gmail.com>
2023-01-16 20:05:39 +0200

    BASE --

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Rectangle Class"""
-from models.base import Base
+from .base import Base
 
 
 class Rectangle(Base):
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Square File"""
-from models.rectangle import Rectangle
+from .rectangle import Rectangle
 
 
 class Square(Rectangle):
