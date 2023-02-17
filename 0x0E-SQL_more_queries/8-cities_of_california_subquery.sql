02097e6db7edbc9e13f74a2b04d130626cd6848e
Yuu <ahmed.elia2003@gmail.com>
2023-02-17 02:17:48 +0200

    testing

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,7 @@
 --cities of cal
-SELECT * FROM STATE
-WHERE name = 'California'
-ORDER BY 
+SELECT id, name FROM cities
+WHERE state_id = (
+	SELECT id FROM states
+	WHERE name = 'California'
+)
+ORDER BY id;
