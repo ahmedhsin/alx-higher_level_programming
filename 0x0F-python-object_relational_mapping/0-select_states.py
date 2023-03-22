db67c80c987d9dc5f841db1f0e3da33131bdf762
Ahmed Mubarak <ahmed.elia2003@gmail.com>
2023-03-22 14:13:30 -0700

    task 0 -- blankline fixing

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Python with sql"""
+"""Python with MySQLdb"""
 from MySQLdb import connect
 import sys
 
@@ -10,7 +10,7 @@ db_name = sys.argv[3]
 
 
 def main():
-    """Main function"""
+    """Main function logic"""
     con = connect(port=3306, user=username, passwd=password, db=db_name)
     cursor = con.cursor()
     sql = "select id, name from states order by id;"
@@ -21,3 +21,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
