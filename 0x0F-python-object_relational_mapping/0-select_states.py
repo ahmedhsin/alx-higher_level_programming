8ac57e5f11d0257ae9b6c442d27df6eb5fe26c00
Ahmed Mubarak <ahmed.elia2003@gmail.com>
2023-03-22 14:07:58 -0700

    inital commit - task 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Python with sql"""
+from MySQLdb import connect
+import sys
+
+
+username = sys.argv[1]
+password = sys.argv[2]
+db_name = sys.argv[3]
+
+
+def main():
+    """Main function"""
+    con = connect(port=3306, user=username, passwd=password, db=db_name)
+    cursor = con.cursor()
+    sql = "select id, name from states order by id;"
+    cursor.execute(sql)
+    for i in range(cursor.rowcount):
+        print(cursor.fetchone())
+
+
+if __name__ == "__main__":
+    main()
