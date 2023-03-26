00ecb3118c6bfdb43883a9b088b173eb91ea885b
Ahmed Mubarak <ahmed.elia2003@gmail.com>
2023-03-26 03:21:11 +0200

    task 7,8,9 --fixing url-sql

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -6,11 +6,13 @@ from sqlalchemy import create_engine
 from sys import argv as keys
 
 if __name__ == '__main__':
-    db_url = 'mysql+pymysql://{}:{}@localhost/{}'.format(
+    db_url = 'mysql+mysqldb://{}:{}@localhost/{}'.format(
         keys[1], keys[2], keys[3])
     engine = create_engine(db_url)
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
     data = session.query(State).order_by(State.id).all()
     for row in data:
         print('{}: {}'.format(row.id, row.name))
+    session.close()
